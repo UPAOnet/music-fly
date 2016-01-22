e42aa9250cd38f31adca9797ae06cc6962e12030
Jordan Chan <jordac67@gmail.com>
2016-01-22 13:57:05 -0800

    issue #3: Added controller and seperate file for soundcloud search request

--- client/app/js/music/spotify-music.js
+++ client/app/js/music/spotify-music.js
@@ -1,4 +1,8 @@
-angular.module('musicApp', []) 
+angular.module('musicApp', [])
+  .controller('scController',  ['$scope', function ($scope) {
+      vm = this;
+      vm.test = 'this is sc';
+  }])
   .controller('spotifyController', ['$scope', '$http', 'spotifySearch', function ($scope, $http, spotifySearch) {
     vm = this;
     vm.tracks = {};
@@ -13,10 +17,7 @@ angular.module('musicApp', [])
         }
       )
     }
-  }])
-  .controller('scController',  ['$scope', function ($scope) {
-      vm = this;
-      vm.test = 'this is sc';
+
   }])
   .directive('musicDirective', function () {
     return {
@@ -25,28 +26,25 @@ angular.module('musicApp', [])
       template: '<button type="button" ng-click="spotifySearch()"/>'
     }
   })
-
-// musicApp.factory('spotifySearch', ['$http', '$q', function ($http, $q) {
-//     var deferred = $q.defer();
-//     var search = {};
-//     // console.log(query)
-//     search.makeRequest = function (input) {
-//       var query = JSON.stringify({queryInput: input})
-//       // console.log(query);
-//       $http({
-//         data: query,
-//         url: 'http://localhost:3000/spotify',
-//         method: 'POST',
-//       }).then(function success (response) {
-//         deferred.resolve(response)
-//         console.log(response);     
-//       })
-//       return deferred.promise;  
-//     } 
-//     return search;
-//   }])
-  
-
+  .factory('spotifySearch', ['$http', '$q', function ($http, $q) {
+    var deferred = $q.defer();
+    var search = {};
+    // console.log(query)
+    search.makeRequest = function (input) {
+      var query = JSON.stringify({queryInput: input})
+      // console.log(query);
+      $http({
+        data: query,
+        url: 'http://localhost:3000/spotify',
+        method: 'POST',
+      }).then(function success (response) {
+        deferred.resolve(response)
+        console.log(response);     
+      })
+      return deferred.promise;  
+    } 
+    return search;
+  }])
 
 
 // 'https://p.scdn.co/mp3-preview/2d89e5af25a276eaf6b9e56baef79a543263afab'
