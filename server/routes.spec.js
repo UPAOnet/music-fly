f6563495028a46393617a58c718970a3b713956b
Jordan Chan <jordac67@gmail.com>
2016-02-03 15:00:20 -0800

    Fixed bug causing route test to be stuck at pending

--- server/routes.spec.js
+++ server/routes.spec.js
@@ -1,15 +1,13 @@
 var expect = require('Chai').expect;
 var request = require('request');
-var fs = require('fs');
 
 describe('Check if Spotify API track search works', function () {
-  it('return a track'), function (done) {
-    var track = '22mek4IiqubGD9ctzxc69s'
-    request('https://api.spotify.com/v1/tracks/22mek4IiqubGD9ctzxc69s' + track, 
+  it('should return a track', function (done) {
+    request('https://api.spotify.com/v1/tracks/22mek4IiqubGD9ctzxc69s', 
       function (error, response, body) {
-        expect(res.statusCode).to.equal(200);
+        expect(response.statusCode).to.equal(200);
         done();
       }
     )
-  }
+  })
 })
