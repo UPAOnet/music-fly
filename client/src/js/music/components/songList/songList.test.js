8e5192066e721190d94975671ae04917c66d86e9
Jordan Chan <jordan@assetavenue.com>
2016-06-19 11:43:52 -0700

    added testing framework

---
+++ client/src/js/music/components/songList/songList.test.js
@@ -0,0 +1,7 @@
+import {songList} from './songList.component.ts';
+
+describe('songList', () => {
+  it('exists', () => {
+    expect(songList).to.exist;
+  })
+})
---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+// const webpackEnv = {test: true};
+const webpackConfig = require('./webpack.config');
+const fileGlob =  './client/src/**/*.test.js';
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [fileGlob],
+    preprocessors: {
+      fileGlob: ['webpack'],
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {noInfo: true},
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      reporters: [
+        {type: 'lcov', dir: 'coverage/', subdir: '.'},
+        {type: 'json', dir: 'coverage/', subdir: '.'},
+        {type: 'text-summary'},
+      ]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
