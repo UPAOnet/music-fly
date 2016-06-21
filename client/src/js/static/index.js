05ca0f9733648c802593ca73401203dcd86959c5
Jordan Chan <jordac67@gmail.com>
2016-06-20 22:12:59 -0700

    Added additional styling

--- client/src/js/static/index.js
+++ client/src/js/static/index.js
@@ -1,4 +1,6 @@
 import {logo} from './logo/logo.ts';
+import {contentOverlay} from './overlays/overlay.ts';
 
 angular.module('musicApp')
-  .component('logo', logo);
+  .component('logo', logo)
+  .component('contentOverlay', contentOverlay)
