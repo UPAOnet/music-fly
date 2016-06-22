c03fdd9d0d07293b02ceb9dd5ce62487e50ff20f
Jordan Chan <jordan@assetavenue.com>
2016-06-22 08:50:10 -0700

    restructured some of hte home page

--- client/src/js/navigation/components/navBar/navComponent.ts
+++ client/src/js/navigation/components/navBar/navComponent.ts
@@ -3,25 +3,38 @@ declare const require: any;
 class Controller {
   private showSideNav: boolean;
   private isOpen;
+  private userScroll: number;
+
   constructor(
     private $scope,
+    private $window,
     private userLogin,
     private $mdSidenav
   ) {
     'ngInject';
 
+    // Services
     this.$mdSidenav = $mdSidenav;
     this.$scope = $scope;
-
+    this.$window = $window;
+    // this.userScroll = $window.pageYOffset;
+    this.checkUserScroll();
   }
 
-  openLeftMenu () {
+  private openLeftMenu () {
     if (this.$mdSidenav('left')) {
       this.showSideNav = !this.showSideNav;
     }
 
     this.$mdSidenav('left').toggle();
   }
+
+  private checkUserScroll () {
+    this.$scope.$watch(() => this.$window.pageYOffset, (newValue, oldValue) => {
+      console.log(this.$window.pageYOffset);
+    })
+  }
+
 }
 
 export const navBar = {
