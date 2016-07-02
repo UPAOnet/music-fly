485bd9bb65d99fd6bf99f60a13641398ead62a65
Jordan Chan <jordan@assetavenue.com>
2016-07-02 12:32:52 -0700

    massive styling to music section for songs

--- client/src/js/static-components/musicPage/musicPageRow.ts
+++ client/src/js/static-components/musicPage/musicPageRow.ts
@@ -1,12 +1,9 @@
 // Row components for the music pages
-
-export const MusicPageRow = function () {
-  return {
-    templateUrl: require('./musicPageRow.html'),
-    transclude: true,
-    restrict: 'A',
-    scope: {
-      header: '@'
-    }
+export const MusicPageRow = {
+  templateUrl: require('./musicPageRow.html'),
+  transclude: true,
+  bindings: {
+    // Binding for any header text
+    header: '@'
   }
 }
---
+++ client/src/js/static/musicPage/MusicPage.ts
@@ -0,0 +1,5 @@
+
+export const MusicPanel = {
+  templateUrl: require('./musicPage.html'),
+
+}
