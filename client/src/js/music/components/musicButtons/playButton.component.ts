ebbe8b46255e3620d3720d33f3a711ffc540b527
Jordan Chan <jordan@assetavenue.com>
2016-07-03 12:05:44 -0700

    Added play button to track lists, need to change tracks into a table

--- client/src/js/music/components/musicButtons/playButton.component.ts
+++ client/src/js/music/components/musicButtons/playButton.component.ts
@@ -1,8 +1,32 @@
 'use strict';
 
-import {ButtonsController} from './PlayerButtons.controller.ts';
+/**
+ * The buttons on the Player
+ * All Button components use this controller
+ * @module playerInterfaceComponent
+ */
 
-declare const require: any;
+export class ButtonsController {
+  private tracks: any;
+  private song: any;
+  private index: number;
+
+  constructor (
+    private TrackList,
+    private $scope,
+    private playerControls
+  ) {
+    'ngInject';
+
+    this.tracks = this.TrackList.currentTracks();
+  }
+
+  // Passses clicked song to the main player
+  public playMusic () {
+    this.playerControls.playMusic(this.song);
+    // this.TrackList.setActive(this.index);
+  }
+}
 
 export const playButton = {
    templateUrl: require('./play-button.html'),
