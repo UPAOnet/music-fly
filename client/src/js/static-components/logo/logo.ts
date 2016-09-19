a4bee77d8da2e3f890ab7f7e6bd85139066bf538
Jordan Chan <jordan@assetavenue.com>
2016-09-18 22:46:48 -0700

    Added reutnr home to logo button

--- client/src/js/static-components/logo/logo.ts
+++ client/src/js/static-components/logo/logo.ts
@@ -1,5 +1,18 @@
 declare const require: any;
 
+class Controller {
+  constructor (
+    private $rootScope:ng.IRootScopeService,
+    private musicEvents
+  ) {
+    'ngInject';
+  }
+  public returnHome () {
+    this.$rootScope.$broadcast(this.musicEvents.featuredPage);
+  }
+}
+
 export const logo = {
+  controller: Controller,
   templateUrl: require('./logo.html')
 }
