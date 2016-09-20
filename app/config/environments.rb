07da5407de7b398ec7d15d9fd1b7fdf99e88a259
Jordan Chan <jordan@assetavenue.com>
2016-09-19 23:38:00 -0700

    getting db ish working

--- app/config/environments.rb
+++ app/config/environments.rb
@@ -1,6 +1,20 @@
-configure :production, :development do
+configure :development do
 
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mf_dev')
+	db = URI.parse('postgres://localhost/mf_dev')
+
+	ActiveRecord::Base.establish_connection(
+			:adapter =>  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
+
+configure :production do
+
+	db = URI.parse(ENV['DATABASE_URL'])
 
 	ActiveRecord::Base.establish_connection(
 			:adapter =>  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
