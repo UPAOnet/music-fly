c501f0a310f57bd8810bb245fa686b49a4bd6065
Jordan Chan <jordan@assetavenue.com>
2016-10-09 13:14:34 -0700

    ignoring typings

--- client/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/es6-promise/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
