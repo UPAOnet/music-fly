bba9110e2ede07bbc54a1db38136a7a229b0ff3d
Jordan Chan <jordan@assetavenue.com>
2017-01-21 15:49:49 -0800

    Added webpack-dev-server and HMR

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,50 +7,65 @@ var path = require('path');
 module.exports = {
   context: __dirname + '/src',
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./entry.js",
+  entry: [
+      "webpack-dev-server/client?http://localhost:8080",
+      "webpack/hot/dev-server",
+      "./entry.js"
+    ],
   output: {
     path: __dirname + "/dev/public",
     filename: "scripts.min.js"
   },
   module: {
    loaders: [
-    {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader!sass-loader'
-    },
-    {
-     test: /\.woff$/,
-     loader: "url-loader?limit=10000&åimetype=application/font-woff&name=[path][name].[ext]"
-    },
-    {
-     test: /\.woff2$/,
-     loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
-    },
-    {
-     test: /\.(eot|ttf|svg|gif|png|jpg)$/,
-     loader: 'file-loader?name=[path][name].[ext]'
-   },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.woff$/,
+        loader: "url-loader?limit=10000&åimetype=application/font-woff&name=[path][name].[ext]"
+      },
+      {
+        test: /\.woff2$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+      },
+      {
+        test: /\.(eot|ttf|svg|gif|png|jpg)$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      },
 
-    {
-      test: /\.ts$/,
-      loader: 'ts'
-    },
-    {
+      {
+        test: /\.ts$/,
+        loader: 'ts'
+      },
+      {
       test: /\.js?$/,
       loader: 'ng-annotate'
-    },
-    {
-      test: /\.html$/,
-      loader: 'file-loader?name=[path][name].[ext]'
-    },
-    {
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      },
+      {
         test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', "stage-0"],
+        }
       }
-    }
-   ]
+   ],
+    devServer: {
+      contentBase: "/client/dev/public",
+      noInfo: true,
+      inline: true,
+      hot: true,
+    },
+    devtool: "eval",
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+    ],
   },
 };
